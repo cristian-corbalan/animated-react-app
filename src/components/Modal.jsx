9fa5c2b95c4fb4fbd3eba907e857518f3ec7514f
cristian-corbalan <ccorbalandw@gmail.com>
2024-09-30 11:27:28 -0300

    Add focus to the modal when it is opened

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,19 @@
+import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 export default function Modal ({ title, children, onClose }) {
+  const modal = useRef();
+
+  useEffect(() => {
+    if (modal.current) {
+      modal.current.focus();
+    }
+  }, [modal]);
+
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <dialog open className="modal" ref={modal}>
         <h2>{title}</h2>
         {children}
       </dialog>
