12dc0919f94d4b9a17a4b309b877ec1585767b52
cristian-corbalan <ccorbalandw@gmail.com>
2024-10-01 15:58:21 -0300

    Add dynamic key to challenge list to re-trigger the animation

--- src/components/Challenges.jsx
+++ src/components/Challenges.jsx
@@ -45,7 +45,7 @@ export default function Challenges () {
           {displayedChallenges.length > 0 && (
             <motion.ol
               className="challenge-items"
-              key="list"
+              key={selectedType}
               initial={{ opacity: 0, y: -20 }}
               animate={{ opacity: 1, y: 0 }}
               exit={{ y: -30, opacity: 0 }}
